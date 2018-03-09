e23f82a716bad3d4197faaa2c8f834e2ab60ceaa
RaynorD <unstoppabledave@gmail.com>
2018-03-09 03:23:20 -0600

    Move back to macros

--- cav_minefield/cfgFunctions.hpp
+++ cav_minefield/cfgFunctions.hpp
@@ -5,10 +5,14 @@ class DOUBLES(PREFIX,COMPONENT) //tag
 {
 	class fnc //category
 	{
-		
-		class initField {file = "cav_minefield\fnc\fn_initField.sqf";};
-		class layField {file = "cav_minefield\fnc\fn_layField.sqf";};
-		class resetField {file = "cav_minefield\fnc\fn_resetField.sqf";};
-		class clearField {file = "cav_minefield\fnc\fn_clearField.sqf";};
+		DEF_FUNC(initField);
+		DEF_FUNC(layField);
+		DEF_FUNC(resetField);
+		DEF_FUNC(clearField);
+		DEF_FUNC(getField);
+		//class initField {file = "cav_minefield\fnc\fn_initField.sqf";};
+		//class layField {file = "cav_minefield\fnc\fn_layField.sqf";};
+		//class resetField {file = "cav_minefield\fnc\fn_resetField.sqf";};
+		//class clearField {file = "cav_minefield\fnc\fn_clearField.sqf";};
 	};
 };
